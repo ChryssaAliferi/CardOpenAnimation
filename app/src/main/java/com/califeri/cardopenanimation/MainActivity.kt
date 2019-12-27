8e0b9b94bb8489481b624079f22bd81ea8cad8d6
Chryssa Aliferi <c.aliferi@gmail.com>
2019-12-27 18:01:19 +0200

    Creates MainActivity.kt activity

--- app/src/main/java/com/califeri/cardopenanimation/MainActivity.kt
+++ app/src/main/java/com/califeri/cardopenanimation/MainActivity.kt
@@ -2,11 +2,22 @@ package com.califeri.cardopenanimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: MaterialButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeViews()
+    }
+
+    private fun initializeViews() {
+        button = findViewById(R.id.mb_start_animation)
+        button.setOnClickListener {
+
+        }
     }
 }
