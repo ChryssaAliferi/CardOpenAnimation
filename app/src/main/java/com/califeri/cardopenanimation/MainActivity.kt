2bcb89a729110ce39d0a5cdf936a688a544eab18
Chryssa Aliferi <c.aliferi@gmail.com>
2019-12-27 18:23:05 +0200

    Start transition on button click

--- app/src/main/java/com/califeri/cardopenanimation/MainActivity.kt
+++ app/src/main/java/com/califeri/cardopenanimation/MainActivity.kt
@@ -2,11 +2,13 @@ package com.califeri.cardopenanimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
 import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var button: MaterialButton
+    private lateinit var rootLayout: MotionLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +17,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initializeViews() {
+        rootLayout = findViewById(R.id.ml_root)
         button = findViewById(R.id.mb_start_animation)
         button.setOnClickListener {
-
+            when (rootLayout.currentState) {
+                R.id.start -> {
+                    rootLayout.transitionToEnd()
+                }
+            }
         }
     }
 }
