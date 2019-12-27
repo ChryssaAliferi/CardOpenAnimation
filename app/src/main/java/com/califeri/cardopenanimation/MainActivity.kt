38cb35e3699e70ea7e5a1d274b258a781cb41b89
Chryssa Aliferi <c.aliferi@gmail.com>
2019-12-27 14:22:39 +0200

    Initial commit

---
+++ app/src/main/java/com/califeri/cardopenanimation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.califeri.cardopenanimation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
